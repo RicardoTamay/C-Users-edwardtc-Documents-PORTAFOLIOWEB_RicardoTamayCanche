f421093c899006c890f587b90c64a9e849f19203
RicardoTamay <ricardotamaycanche047@gmail.com>
2022-11-01 15:12:05 -0500

    Se modican algunos codigos, donde se agrgaron FLEXBOX, BOOTSTRATP, JAVASCRIPT En algunas partes del pagina web

---
+++ js/Script.js
@@ -0,0 +1,11 @@
+ {
+    alert("BIENVENIDOS A MI MUNDO")
+}
+Swal.fire({
+    title: "HOLA!!!!!!",
+    text: "Espero que te guste mi pagina web",
+    icon: 'success',
+    timer: 5000,
+    timerProgressBar: true,
+
+})
